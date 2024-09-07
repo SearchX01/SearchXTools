c8d654b1dcad6d023f946a01f0d8086023cb862f
SearchX <robertkeuss@gmail.com>
2024-09-07 16:16:07 +0200

    Add files via upload

---
+++ src/codegpt.py
@@ -0,0 +1,39 @@
+import os
+import base64
+import fade
+import colorama
+from colorama import Fore
+
+
+colorama.init(autoreset=True)
+
+def clear_screen():
+    """Efface l'écran du terminal."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+text = fade.greenblue("""
+       ▄████████    ▄████████    ▄████████    ▄████████  ▄████████    ▄█    █▄    ▀████    ▐████▀ 
+      ███    ███   ███    ███   ███    ███   ███    ███ ███    ███   ███    ███     ███▌   ████▀  
+      ███    █▀    ███    █▀    ███    ███   ███    ███ ███    █▀    ███    ███      ███  ▐███    
+      ███         ▄███▄▄▄       ███    ███  ▄███▄▄▄▄██▀ ███         ▄███▄▄▄▄███▄▄    ▀███▄███▀    
+    ▀███████████ ▀▀███▀▀▀     ▀███████████ ▀▀███▀▀▀▀▀   ███        ▀▀███▀▀▀▀███▀     ████▀██▄     
+             ███   ███    █▄    ███    ███ ▀███████████ ███    █▄    ███    ███     ▐███  ▀███    
+       ▄█    ███   ███    ███   ███    ███   ███    ███ ███    ███   ███    ███    ▄███     ███▄  
+     ▄████████▀    ██████████   ███    █▀    ███    ███ ████████▀    ███    █▀    ████       ███▄ 
+                                                                 CODEGPT""")
+print(text)
+
+
+menu = fade.greenblue("""CODE GPT : https://gptcall.net/chat?data=%7B%22contact%22%3A%7B%22id%22%3A%22tjFLHnVJB_V0hpSvSzyBc%22%2C%22flow%22%3Atrue%7D%7D#chatID=%222024-09-07T13%3A35%3A36.489Z%22""")
+print(menu)
+
+def main():
+   
+    input(Fore.GREEN + "\nAppuyez sur Entrée pour quitter...")
+
+    
+    clear_screen()
+
+if __name__ == "__main__":
+    main()
---
+++ src/iajailbreak.py
@@ -0,0 +1,39 @@
+import os
+import base64
+import fade
+import colorama
+from colorama import Fore
+
+
+colorama.init(autoreset=True)
+
+def clear_screen():
+    """Efface l'écran du terminal."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+text = fade.greenblue("""
+       ▄████████    ▄████████    ▄████████    ▄████████  ▄████████    ▄█    █▄    ▀████    ▐████▀ 
+      ███    ███   ███    ███   ███    ███   ███    ███ ███    ███   ███    ███     ███▌   ████▀  
+      ███    █▀    ███    █▀    ███    ███   ███    ███ ███    █▀    ███    ███      ███  ▐███    
+      ███         ▄███▄▄▄       ███    ███  ▄███▄▄▄▄██▀ ███         ▄███▄▄▄▄███▄▄    ▀███▄███▀    
+    ▀███████████ ▀▀███▀▀▀     ▀███████████ ▀▀███▀▀▀▀▀   ███        ▀▀███▀▀▀▀███▀     ████▀██▄     
+             ███   ███    █▄    ███    ███ ▀███████████ ███    █▄    ███    ███     ▐███  ▀███    
+       ▄█    ███   ███    ███   ███    ███   ███    ███ ███    ███   ███    ███    ▄███     ███▄  
+     ▄████████▀    ██████████   ███    █▀    ███    ███ ████████▀    ███    █▀    ████       ███▄ 
+                                                                 DARKGPT""")
+print(text)
+
+
+menu = fade.greenblue("""CODE GPT : https://github.com/friuns2/BlackFriday-GPTs-Prompts/blob/main/Jailbreaks.md""")
+print(menu)
+
+def main():
+   
+    input(Fore.GREEN + "\nAppuyez sur Entrée pour quitter...")
+
+    
+    clear_screen()
+
+if __name__ == "__main__":
+    main()
