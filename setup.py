d4b81660984c006ab3bce2b15d9056786f353ad3
SearchX <robertkeuss@gmail.com>
2024-09-08 12:41:16 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ try:
     print("Installing the python modules required for the tools:")
 
     if sys.platform.startswith("win"):
-        os.system("python -m pip install --upgrade pip")
-        os.system("python -m pip install -r requirements.txt")
+        os.system("pip install -r requirements.txt")
         os.system("python main.py")
 
     elif sys.platform.startswith("linux"):
         os.system("python3 -m pip3 install --upgrade pip")
-        os.system("python3 -m pip3 install -r requirements.txt")
+        os.system("pip3 install -r requirements.txt")
         os.system("python3 main.py")
 
 except Exception as e:
