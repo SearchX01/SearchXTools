81194c373265cdf297cd296bf4460bfcc204cc9a
SearchX <robertkeuss@gmail.com>
2024-09-08 13:05:20 +0200

    update.py

--- src/update.py
+++ src/update.py
@@ -8,23 +8,20 @@ import subprocess
 repo_url = "https://github.com/SearchX01/SearchXTools/"
 destination_src = "src"
 destination_main = "."
+requirements_file = os.path.join(destination_main, "requirements.txt")
 
 def clear_console():
-    
     if platform.system() == "Windows":
         os.system('cls')
     else:
         os.system('clear')
 
 def on_rm_error(func, path, exc_info):
-   
-    os.chmod(path, stat.S_IWRITE)  
+    os.chmod(path, stat.S_IWRITE)
     os.remove(path)
 
 def install_git_if_missing():
-    """Installer Git si nécessaire. Fonctionne sous Linux et Windows."""
     try:
-        
         subprocess.run(["git", "--version"], check=True)
     except (subprocess.CalledProcessError, FileNotFoundError):
         if platform.system() == "Windows":
@@ -39,7 +36,6 @@ def install_git_if_missing():
 def update_files(repo_url, destination_src, destination_main):
     temp_dir = os.path.join(os.getcwd(), "temp_clone")
     
-   
     if os.path.exists(temp_dir):
         shutil.rmtree(temp_dir, onerror=on_rm_error)
 
@@ -50,12 +46,12 @@ def update_files(repo_url, destination_src, destination_main):
 
         repo_src_dir = os.path.join(temp_dir, "src")
         repo_main_path = os.path.join(temp_dir, "main.py")
+        repo_requirements_path = os.path.join(temp_dir, "requirements.txt")
 
         if os.path.exists(repo_src_dir):
             for item in os.listdir(repo_src_dir):
                 source_path = os.path.join(repo_src_dir, item)
                 destination_path = os.path.join(destination_src, item)
-                
                 
                 if os.path.isfile(source_path) and (item.endswith(".py") or item.endswith(".txt")):
                     shutil.copy2(source_path, destination_path)
@@ -73,6 +69,12 @@ def update_files(repo_url, destination_src, destination_main):
             print("Fichier main.py mis à jour.")
         else:
             print("Le fichier main.py n'existe pas dans le dépôt cloné.")
+
+        if os.path.exists(repo_requirements_path):
+            shutil.copy2(repo_requirements_path, requirements_file)
+            print("Le fichier requirements.txt a été remplacé avec succès.")
+        else:
+            print("Le fichier requirements.txt n'existe pas dans le dépôt cloné.")
     
     except Exception as e:
         print(f"Erreur lors du clonage ou de la copie des fichiers : {e}")
@@ -84,11 +86,6 @@ def update_files(repo_url, destination_src, destination_main):
             shutil.rmtree(temp_dir, onerror=on_rm_error)
         print("Mise à jour terminée et dossier temporaire supprimé.")
 
-
 install_git_if_missing()
-
-
 update_files(repo_url, destination_src, destination_main)
-
-
 clear_console()
